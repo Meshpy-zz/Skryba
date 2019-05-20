6778c03883c9a296a6e93bbf36662fd6be29c8e3
Patryk Gitner <209317@edu.p.lodz.pl>
2019-05-20 11:41:33 +0200

    Dodanie 4 ekranu z wysłaniem publikacji

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {BrowserComponent} from './components/browser/browser.component';
 import {MainPageComponent} from './components/main-page/main-page.component';
+import {AddFileComponent} from './components/add-file/add-file.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'browser', component: BrowserComponent },
-  { path: 'main', component: MainPageComponent }
+  { path: 'main', component: MainPageComponent },
+  { path: 'add-file', component: AddFileComponent }
 ];
 
 @NgModule({
