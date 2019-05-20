de60f6457a0f15b28217e6129680bef4d5b80c1d
Patryk Gitner <209317@edu.p.lodz.pl>
2019-05-20 11:18:50 +0200

    Dodanie drugiego widoku po zalogowaniu użytkownika

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {BrowserComponent} from './components/browser/browser.component';
+import {MainPageComponent} from './components/main-page/main-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'browser', component: BrowserComponent }
+  { path: 'browser', component: BrowserComponent },
+  { path: 'main', component: MainPageComponent }
 ];
 
 @NgModule({
