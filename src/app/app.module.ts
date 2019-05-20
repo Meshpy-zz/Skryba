aaa717b7597e88a3e0f846071e602accb75e00f2
Patryk Gitner <209317@edu.p.lodz.pl>
2019-05-20 10:32:33 +0200

    Dodanie 4 wygenerowanych komponentów odzwierciedlających widoki strony w celu stworzenia makiet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeNavbarComponent } from './components/home-navbar/home-navbar.component';
+import { UserNavbarComponent } from './components/user-navbar/user-navbar.component';
+import { BrowserComponent } from './components/browser/browser.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { AddFileComponent } from './components/add-file/add-file.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeNavbarComponent,
+    UserNavbarComponent,
+    BrowserComponent,
+    MainPageComponent,
+    AddFileComponent
   ],
   imports: [
     BrowserModule,
